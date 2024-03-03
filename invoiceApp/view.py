5f9e83408c36031205e9bc2a191dbb6016bf0200
AmritaValueCS-22 <virtual_labs@am.amrita.edu>
2024-03-03 15:05:28 +0530

    new changes

--- invoiceApp/view.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-
-def firstApi(req):
-    html = "<html><body>My first api</body></html>"
-    return HttpResponse(html)
-
-def dynamicApi(req,name):
-    return HttpResponse("my name is " +name+" >>>>")
